670dfb53fae2638e75cc7863a4416ceaf15c8b5a
945100985@qq.com <945100985@qq.com>
2026-09-11 14:00:27 +0800

    commit

--- blurDemo/build.gradle.kts
+++ blurDemo/build.gradle.kts
@@ -43,9 +43,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("com.google.android.material:material:1.10.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 
     implementation("com.github.Dimezis:blurview:version-3.2.0")
 }
